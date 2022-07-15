09da0f5fd688c68bca0df3553ebf4fdfdfd92049
jdi022222 <jdi022222@gmail.com>
2022-07-15 16:27:06 +0900

    테스트를 위한 입출력 리다이렉팅

---
+++ src/test/java/com/ll/exam/AppTest.java
@@ -0,0 +1,27 @@
+package com.ll.exam;
+
+import org.testng.annotations.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+    @Test
+    public void 스캐너에_키보드가_아닌_문자열을_입력으로_설정() {
+        Scanner sc = TestUtil.genScanner("안녕");
+
+        String cmd = sc.nextLine().trim();
+        assertEquals("안녕", cmd);
+    }
+    @Test
+    public void 출력을_모니터에_하지_않고_문자열로_얻기() {
+        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+        System.out.print("안녕");
+        String rs = output.toString();
+        TestUtil.clearSetOutToByteArray(output);
+
+        assertEquals("안녕", rs);
+    }
+}
