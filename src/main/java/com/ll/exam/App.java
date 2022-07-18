d25d7222e42633bd58db75b55cc7902402164cfa
jdi022222 <jdi022222@gmail.com>
2022-07-18 09:22:45 +0900

    목록 구현

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -1,5 +1,7 @@
 package com.ll.exam;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -13,11 +15,12 @@ public class App {
         System.out.println("== 명언 SSG ==");
 
         int wiseSayingsLastId = 0;
+        List<WiseSaying> wiseSayings = new ArrayList<>();
+
         outer:
         while (true) {
-            System.out.print("명령)");
+            System.out.print("명령) ");
             String cmd = sc.nextLine().trim();
-
             switch (cmd) {
                 case "등록":
                     int id = ++wiseSayingsLastId;
@@ -25,7 +28,20 @@ public class App {
                     String content = sc.nextLine();
                     System.out.print("작가 : ");
                     String author = sc.nextLine();
+
+                    wiseSayings.add(new WiseSaying(id, content, author));
+
                     System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+                    break;
+                case "목록":
+                    System.out.println("번호 / 작가 / 명언");
+                    System.out.println("----------------------");
+
+                    for (int i = wiseSayings.size() - 1; i >= 0; i--) {
+                        WiseSaying wiseSaying = wiseSayings.get(i);
+
+                        System.out.printf("%d / %s / %s\n", wiseSaying.id, wiseSaying.author, wiseSaying.content);
+                    }
                     break;
                 case "종료" :
                     break outer;
